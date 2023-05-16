8d31649ed839d53ba003ebb1c1400e5a03215fee
Gao Sion <gaoziyuan19@mails.ucas.ac.cn>
2023-05-16 15:45:21 +0800

    update docs

---
+++ scripts/qemu_debug.sh
@@ -0,0 +1,11 @@
+qemu-system-riscv64 \
+    -machine virt \
+    -kernel kernel-qemu \
+    -m 128M \
+    -nographic \
+    -smp 2 \
+    -bios sbi-qemu \
+    -drive file=sdcard.img,if=none,format=raw,id=x0 \
+    -device virtio-blk-device,drive=x0,bus=virtio-mmio-bus.0
+    -gdb tcp::1234 \
+    -S
