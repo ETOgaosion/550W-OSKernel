f032223d5e419223da8348fcd461e15759f98b4d
Gao Sion <gaoziyuan19@mails.ucas.ac.cn>
2023-05-16 18:49:00 +0800

    sd card can be written

--- scripts/qemu_debug.sh
+++ scripts/qemu_debug.sh
@@ -7,5 +7,5 @@ qemu-system-riscv64 \
     -bios default \
     -drive file=sdcard.img,if=none,format=raw,id=x0 \
     -device virtio-blk-device,drive=x0,bus=virtio-mmio-bus.0
-    -gdb tcp::1234 \
+    -s \
     -S
