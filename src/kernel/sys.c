0efec3b4d3b04999c40d2eea22361ee31c52b047
Gao Sion <gaoziyuan19@mails.ucas.ac.cn>
2023-05-17 17:35:50 +0800

    move pipe2 to fs, add uname

---
+++ src/kernel/sys.c
@@ -0,0 +1,16 @@
+#include <os/sys.h>
+#include <lib/string.h>
+
+uname_t uname_550w = {
+    .sysname = "MOSS",
+    .nodename = "oscomp",
+    .release = "1.0.0",
+    .version = "#1",
+    .machine = "550W",
+    .domainname = "bluespace.moss.com"
+};
+
+long sys_uname(uname_t *dest) {
+    memcpy((uint8_t *)dest, (uint8_t *)&uname_550w, sizeof(uname_550w));
+    return 0;
+}
