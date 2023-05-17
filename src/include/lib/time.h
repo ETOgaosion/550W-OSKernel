368f1e15302c4ab5abc6f6472ccce17f56d09db4
Gao Sion <gaoziyuan19@mails.ucas.ac.cn>
2023-05-17 13:58:20 +0800

    seperate all syscall to different modules

--- src/include/lib/time.h
+++ src/include/lib/time.h
@@ -7,9 +7,9 @@ extern uint64_t time_elapsed;
 extern list_head timers;
 
 uint64_t get_timer(void);
-uint64_t get_ticks(void);
+uint64_t sys_get_ticks(void);
 uint32_t get_wall_time(uint32_t *);
 
-uint64_t get_time_base();
+uint64_t sys_get_time_base();
 
 void latency(uint64_t time);
